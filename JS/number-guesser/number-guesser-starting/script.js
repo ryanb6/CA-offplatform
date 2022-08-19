c82b8dc5bcdb53dcff4e3ef5ee4d8197d4ff28a6
unknown <arbef161@gmail.com>
2022-08-19 15:34:42 -0400

    completed number guesser

--- JS/number-guesser/number-guesser-starting/script.js
+++ JS/number-guesser/number-guesser-starting/script.js
@@ -4,3 +4,46 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// Get random number
+const generateTarget = () => random(1,9);
+
+// Generates number between 1 and 9
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// Find difference between guess and target, compare results
+const compareGuesses = (humanGuess, compGuess, secretTarget) => {
+    let humanDiff = Math.abs(secretTarget - humanGuess);
+    let compDiff = Math.abs(secretTarget - compGuess);
+    if (humanDiff > compDiff) {
+        return false;
+    } else if (humanDiff < compDiff ) {
+        return true;
+    } else {
+        return true;
+    }
+}
+
+const updateScore = (winner) => {
+    switch (winner) {
+        case 'human': 
+            humanScore += 1;
+            break;
+        case 'computer':
+            computerScore += 1;
+            break;
+    }
+}
+
+const advanceRound = () => currentRoundNumber += 1;
+
+
+// Debugging
+/*
+let hGuess = generateTarget();
+let cGuess = generateTarget();
+let target = generateTarget();
+
+let win = compareGuesses(hGuess, cGuess, target);
+
+console.log(`${win}, hguess ${hGuess} cguess ${cGuess} target ${target}`);
+*/
