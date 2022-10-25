437e55fe8e227b7e579474641dde9cdaad5c0ebc
unknown <arbef161@gmail.com>
2022-10-25 16:27:02 -0400

    added random.js

---
+++ JS/random.js
@@ -0,0 +1,11 @@
+console.log("I'm learning about");
+ 
+for (let idx=0; idx < 999999999; idx++) {}
+ 
+// The second console.log() statement is
+// delayed by the for loop's execution
+console.log("the Event Loop");
+
+console.log("I’m learning about");
+setTimeout(() => { console.log("Event Loop");}, 2000);
+console.log("the");
